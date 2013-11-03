d403ca2f3e241f982fb617f4158c384c77d78db0
Yitong Zhou <timyitong@gmail.com>
2013-11-03 17:27:24 -0500

    models

--- mysql_models.js
+++ mysql_models.js
@@ -1,7 +1,15 @@
 module.exports = function(app, Sequelize){
     this.users = app.sequelize.define('users', {
-        uid : Sequelize.STRING,
- 
+        uid : Sequelize.INTEGER,
+        username : Sequelize.STRING,
+        password : Sequelize.STRING,
+        type     : Sequelize.STRING,
+    });
+
+    this.slides = app.sequelize.define('slides', {
+        sid  : Sequelize.INTEGER,
+        text : Sequelize.TEXT,
+    })
 // End of this file
     return this;
 }
